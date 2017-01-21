387028af5bd811876978491aa162a9ec398be4e0
collinG123 <collin.gustafson@gmail.com>
2017-01-21 10:51:23 -0600

    updated

--- FTC745RobitShootTestv1_0.java
+++ FTC745RobitShootTestv1_0.java
@@ -1,15 +1,13 @@
 /**
-Made by Daylan Davis and Collin Gustafson
-Captained by Ryan Davitt
-This program is the first TeleOp version for the FTC 11745 team of 2016-17 Slappy Robit 2.0.
-This program (version 1.0) was originally used for Slappy 1.0 until the Scrimmage of '16, when the team made a total overhaul of Slappy 1.0.
-Slappy 2.0 now resides in the Woodrow Wilson High School Robotics Room. Slappy 1.0 resides in soul.
-
-@Verison 2.1.1
-
-Version 2.1- RELEASE
-Version 2.1.1- Tweaked gear numbers to acommadate for motor power curve
-**/
+ Made by Daylan Davis and Collin Gustafson
+ Captained by Ryan Davitt
+ This program is the first TeleOp version for the FTC 11745 team of 2016-17 Slappy Robit 2.0.
+ This program (version 1.0) was originally used for Slappy 1.0 until the Scrimmage of '16, when the team made a total overhaul of Slappy 1.0.
+ Slappy 2.0 now resides in the Woodrow Wilson High School Robotics Room. Slappy 1.0 resides in soul.
+ @Verison 2.1.1
+ Version 2.1- RELEASE
+ Version 2.1.1- Tweaked gear numbers to acommadate for motor power curve
+ **/
 
 package org.firstinspires.ftc.teamcode;
 
@@ -26,8 +24,8 @@ import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.teamcode.FTC745Lib.FTC745Drive_v2_0_RELEASE;
-import static org.firstinspires.ftc.teamcode.FTC745Lib.FTC745Drive_v2_0_RELEASE.Shooting.ParticleShootTele;
+import org.firstinspires.ftc.teamcode.FTC745Lib.FTC745Drive_v2_1_DEV;
+import static org.firstinspires.ftc.teamcode.FTC745Lib.FTC745Drive_v2_1_DEV.Shooting.ParticleShootTele;
 
 
 @TeleOp(name="Shoot Test v1.0", group="TeleOp")  // @Autonomous(...) is the other common choice
@@ -84,10 +82,9 @@ public class FTC745RobitShootTestv1_0 extends LinearOpMode {
                 motorRshoot.setPower(0);
                 SystemClock.sleep(200);
                 idle();
-        }
-        idle();
+            }
+            idle();
         }
         idle();
     }
 }
-
