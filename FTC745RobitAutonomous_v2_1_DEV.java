ef82858b46779070fdccbf259500c0524cde46e3
Rydogdav <rydogdav@gmail.com>
2017-02-20 16:59:41 -0600

    2-20-17

--- FTC745RobitAutonomous_v2_1_DEV.java
+++ FTC745RobitAutonomous_v2_1_DEV.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -10,6 +11,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 
 @Autonomous(name="Auto v2.1 DEV", group="Autonomous")
+@Disabled
 
 public class FTC745RobitAutonomous_v2_1_DEV extends LinearOpMode {
     DcMotor motorFRight;
